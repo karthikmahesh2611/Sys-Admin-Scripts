faeb2dbd5b58a4926c4ac8a6418ad78d30d35719
Karthik Mahesh <39571363+karthikmahesh2611@users.noreply.github.com>
2018-08-01 09:06:17 +0530

    Update change_file_names.py

--- Change File Extensions/change_file_names.py
+++ Change File Extensions/change_file_names.py
@@ -6,6 +6,10 @@ import sys
 import argparse
 import re
 
+#Get the required arguments from the user which includes:
+# Directory Path
+# Old File Extension
+# Nex File Extension
 parser = argparse.ArgumentParser()
 parser.add_argument("Path", help="Directory Path")
 parser.add_argument("ExtA", help="Old Extension to be changed. Ex: txt")
@@ -13,15 +17,19 @@ parser.add_argument("ExtB", help="New File Extension. Ex: txt")
 
 args = parser.parse_args()
 
-#Regular expression with greedy match
+#Regular expression with greedy match filenames.ext
 file1 = r"(.*)(\.)(" + args.ExtA + ")"
 re_obj = re.compile(file1)
 
+#os.walk will find all directories and files in the mentoned directory path
 for dirpath, dirnames, filenames in os.walk(args.Path):
 	for file in filenames:
 		oldpath = os.path.join(dirpath, file)
+		#Subsitute the new extenison using re 'sub' method
 		new_file = re_obj.sub(r'\1\2' + args.ExtB, file)
 		newpath = os.path.join(dirpath, new_file)
+		#Rename the files
 		os.rename(oldpath, newpath)
 
-print('Successfully completed')
+#Script completed successfully
+print('\nSuccessfully completed')
