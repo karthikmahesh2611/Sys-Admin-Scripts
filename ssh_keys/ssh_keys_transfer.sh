ef2a12e0dc07dec4f599b6c5505b768e1976ebc0
Karthik Mahesh <39571363+karthikmahesh2611@users.noreply.github.com>
2018-10-15 07:26:58 +0530

    Update ssh_keys_transfer.sh

--- ssh_keys/ssh_keys_transfer.sh
+++ ssh_keys/ssh_keys_transfer.sh
@@ -2,5 +2,5 @@
 
 for server in $(awk 'NR > 2 {print $1}' /etc/hosts)
 do
-cat ~/.ssh/id_rsa.pub | ssh root@$server "mkdir -p .ssh; cat >> .ssh/authorized_keys; chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
+cat ~/.ssh/id_rsa.pub | ssh $USER@$server "mkdir -p .ssh; cat >> .ssh/authorized_keys; chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 done
